7f540adc045d91c2d1592e779a03222ae6f07461
unlocomqx <amxmodx.unloco@gmail.com>
2021-07-28 04:09:58 +0100

    Complete test

--- src/lib/components/Button.spec.tsx
+++ src/lib/components/Button.spec.tsx
@@ -1,9 +1,8 @@
-import Button from "./Button.svelte";
 import { render } from "@testing-library/svelte";
+import Button from "./Button.svelte";
 
 it("should prefetch local link", () => {
-  render(<Button>ABCD</Button>)
+  render(<Button>ABCD</Button>);
 
-  cy
-  .get('button')
+  cy.get("button").should("have.length", 1);
 });
